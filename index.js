f5af0b1c23f7c3e88053e23e46736cb1ba5ca084
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-03-31 10:58:36 -0300

    methods Get adding

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-const db = require("./db");
-console.log("Connecting to database........")
+const {Pool} = require('pg')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
 
+let pool = new Pool();
+
+app.use(morgan('dev'));
+
+const port = process.env.PORT;
+app.listen(port, function () {
+    console.log(`Server started on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form method="GET" action="info/get"></form>
+    </body>
+    </html>`)
+});
+
+app.get("/info/get", (req, res) => {
+    try {
+            pool.connect((error, client, release) => {
+            let resp = client.query("SELECT * FROM clientes;")
+            res.send(resp.rows);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+})
