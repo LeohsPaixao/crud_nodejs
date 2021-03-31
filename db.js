fd0b8ea64307ef3932472000a65d00dd7482c775
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-03-31 15:29:51 -0300

    new connections

---
+++ db.js
@@ -0,0 +1,8 @@
+const cliente = require('pg').Client
+const db = new cliente({
+    host: 'localhost',
+    user: 'postgres',
+    password: '96127748',
+    port: 5432,
+    database: 'crudnode'
+})
