1ba3609443be7542e1f025bde06693beaafa540f
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-01 18:08:07 -0300

    creating routes admin and delete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 require('dotenv').config();
+const route = require("./routes/routes.js");
 
 
 app.use(morgan('dev'));
@@ -11,84 +12,16 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-const port = process.env.PORT;
-app.listen(port, function () {
-    console.log(`Server started on port ${port}`);
-});
-
-app.get('/', (red, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Cadastrar Clientes</title>
-    </head>
-    
-    <body>
-        <h1 id="title">Cadastrar Clientes</h1>
-        <form action="/info/add" method="POST">
-            <label for="add">CADASTRAR: </label>
-            <table>
-                <tr>
-                    <td><input type="text" name="id" id="id"></td>
-                </tr>
-                <tr>
-                    <td><input type="text" name="name" id="name"></td>
-                </tr>
-                <tr>
-                    <td><input type="text" name="phone" id="phone"></td>
-                </tr>
-                <tr>
-                    <td><input type="email" name="email" id="email"></td>
-                </tr>
-                <tr>
-                    <td><input type="text" name="idade" id="idade"></td>
-                </tr>
-            </table><br>
-            <input type="submit" value="REGISTRAR">
-        </form>
-        <br>
-        <form action="/info/delete" method="POST">
-            <label for="delete">DELETAR: </label>
-            <table>
-                <tr>
-                    <td><input type="text" name="id" id="id"></td>
-                </tr>
-            </table>
-            <input type="submit" value="DELETAR">
-        </form>
-    </body>
-    
-    </html>`);
-});
-
-app.get("/info/get", (req, res) => {
-    try {
-        db.connect();
-        let resp = db.query(`SELECT * FROM clientes`);
-        res.send(resp.rows);
-        release();
-
-    } catch (ex) {
-        console.error(404);
-    }
-
-});
 
 app.post("/info/add", (req, res) => {
     try {
         db.connect();
-        let resp = db.query(`INSERT INTO clientes (id, name, phone, email, idade) VALUES ('${req.body.id}', '${req.body.name}', '${req.body.phone}', '${req.body.email}', '${req.body.year}')`);
+        let resp = db.query(`INSERT INTO clientes (id, name, phone, email, idade) VALUES (${req.body.id}, '${req.body.name}', '${req.body.phone}', '${req.body.email}', '${req.body.idade}')`);
         console.log(resp);
         res.redirect('/info/get');
 
     } catch (ex) {
         console.error(404);
-    } finally {
-        db.end();
-        console.log("Disconnected!!");
     }
 
 });
@@ -102,9 +35,6 @@ app.post("/info/delete", (req, res) => {
 
     } catch (ex) {
         console.error(404);
-    } finally {
-        db.end();
-        console.log("Disconnected!!");
     }
 
 });
@@ -112,15 +42,20 @@ app.post("/info/delete", (req, res) => {
 app.post("/info/update", (req, res) => {
     try {
         db.connect();
-        let resp = db.query(`UPDATE clientes SET id = '${req.body.newValue}' WHERE id = '${req.body.oldValue}'`);
+        let resp = db.query(`UPDATE clientes SET id = '${req.body.odl_id}' WHERE id = '${req.body.new_id}'`);
         console.log(resp);
         res.redirect('/info/get');
 
     } catch (ex) {
-        console.error(404);
-    } finally {
-        db.end();
-        console.log("Disconnected!!");
+        console.log(404);
     }
 
-})
+});
+// Routes
+app.use('/admin', route);
+
+// connect in the Port to Node
+const port = process.env.PORT;
+app.listen(port, function () {
+    console.log(`Server started on port ${port}`);
+});
